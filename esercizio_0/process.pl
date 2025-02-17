6b4da439c5852e9977c99e546819822416a09f33
Mariano Spadaccini <spadacciniweb@gmail.com>
2025-02-17 15:42:06 +0100

    Cleanup process.pl

--- esercizio_0/process.pl
+++ esercizio_0/process.pl
@@ -32,21 +32,21 @@ my $dsn = sprintf("dbi:mysql:dbname=%s;host=%s;port=%s;",
 my $dbh = DBI->connect($dsn, $ENV{DB_USER}, $ENV{DB_PWD})
             or die "Connection error: $DBI::errstr";
 
-my $dt_start = DateTime->now;
+my $dt_start = time();
 
 my %user = ();
 
 process_movements();
 update_accounts();
 
-my $dt_end = DateTime->now;
-my_log( sprintf "END in %s seconds", ($dt_end - $dt_start)->seconds )
+my $delta_t = time() - $dt_start;
+my_log( sprintf "END in %d minutes %d seconds", $delta_t / 60, $delta_t % 60 )
     if $ENV{DEBUG};
 exit 0;
 
 sub my_log {
     $_ = shift;
-    printf "[%s] %s\n", DateTime->now, $_ || '-> missing <-';
+    printf "[%s] %s\n", DateTime->now->datetime(' '), $_ || '-> missing <-';
 }
 
 sub process_movements {
